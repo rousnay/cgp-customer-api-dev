c6d9063d867ca3075dd304c7c3592767e1a69735
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-12-13 18:30:59 +0600

    Config modified

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,7 @@ async function bootstrap() {
 bootstrap();
 
 // require('dotenv').config()
-
 // const mysql = require('mysql2')
-
 // // Create the connection to the database
 // const connection = mysql.createConnection(process.env.DATABASE_URL)
 
