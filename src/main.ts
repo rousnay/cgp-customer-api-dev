3b8c2ae5ab43a5bf79834636fd2d89aee4a76df7
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-12-13 17:58:25 +0600

    API Doc modified

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Demography API')
+    .setDescription('OpenAPI Specification for Demography API build by Revinr')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('The Documentation')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
