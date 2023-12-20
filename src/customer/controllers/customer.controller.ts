4c1dd7f493d7ce2edba9d0ac7ee523c22c7af2fe
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-12-20 19:40:23 +0600

    some more code refactoring

--- src/customer/controllers/customer.controller.ts
+++ src/customer/controllers/customer.controller.ts
@@ -11,14 +11,76 @@ import {
 import { CustomerService } from '../services/customer.service';
 import { CreateCustomerDto } from '../dtos/create-customer.dto';
 import { Customer } from '../entities/customer.entity';
-import { ApiBody, ApiConsumes, ApiTags, ApiQuery } from '@nestjs/swagger';
+import {
+  ApiHeader,
+  ApiOperation,
+  ApiBody,
+  ApiConsumes,
+  ApiParam,
+  ApiResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiQuery,
+} from '@nestjs/swagger';
 
+// @ApiHeader({
+//   name: 'X-MyHeader',
+//   description: 'Custom header',
+// })
 @Controller('customer')
 @ApiTags('Customer')
 export class CustomerController {
   constructor(private customerService: CustomerService) {}
 
+  // Get all customers ++++++++++++++++++++++++++++++++++++
   @Get('all')
+  @ApiOperation({
+    summary: 'Get all customers',
+    // description: 'Returns an example response',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of customers',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            message: { type: 'string', example: 'This is an example message' },
+            data: {
+              type: 'object',
+              properties: {
+                id: { type: 'number', example: 1 },
+                name: { type: 'string', example: 'John Doe' },
+              },
+            },
+          },
+        },
+        example: {
+          data: [
+            {
+              id: 1,
+              sosVoterId: '2119833852',
+              idNumber: 4336257,
+              voterStatus: 'A',
+              partyCode: ' ',
+              firstName: 'ATIF',
+              middleName: 'ATIQ',
+              lastName: 'ABBASI',
+              sex: 'M',
+              birthDate: '1973',
+              streetName: 'FOREST',
+              streetNumber: '9302',
+              streetType: 'LN',
+              city: 'DALLAS',
+              zipCode: '75243',
+            },
+          ],
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @ApiQuery({ name: 'page', required: false, type: Number })
   @ApiQuery({ name: 'limit', required: false, type: Number })
   public async getCustomers(
@@ -36,19 +98,78 @@ export class CustomerController {
     return customers;
   }
 
+  // Get a customer by ID ++++++++++++++++++++++++++++++++++
   @Get('/:customerId')
+  @ApiOperation({ summary: 'Get a customer by ID' })
+  @ApiParam({ name: 'customerId', type: Number })
+  @ApiResponse({ status: 200, type: Customer })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   public async getCustomer(@Param('customerId') customerId: number) {
     return await this.customerService.getCustomer(customerId);
   }
 
+  // Create a new customer ++++++++++++++++++++++++++++++++
   @Post('create')
+  @ApiOperation({
+    summary: 'Create a new customer',
+    // description: 'Returns an example response',
+  })
+  @ApiBody({ type: CreateCustomerDto })
+  @ApiResponse({
+    status: 201,
+    type: Customer,
+    description: 'The created customer',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            message: { type: 'string', example: 'This is an example message' },
+            data: {
+              type: 'object',
+              properties: {
+                id: { type: 'number', example: 1 },
+                name: { type: 'string', example: 'John Doe' },
+              },
+            },
+          },
+        },
+        example: {
+          message: 'This is an example message',
+          data: {
+            sosVoterId: 'ABC123',
+            idNumber: 12345,
+            voterStatus: 'Active',
+            partyCode: 'XYZ',
+            firstName: 'John',
+            middleName: 'M',
+            lastName: 'Doe',
+            sex: 'Male',
+            birthDate: '1990-01-01',
+            streetName: 'Main Street',
+            streetNumber: '123',
+            streetType: 'Ave',
+            city: 'Cityville',
+            zipCode: '12345',
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   public async createCustomer(
     @Body() createCustomerDto: CreateCustomerDto,
   ): Promise<Customer> {
     return await this.customerService.createCustomer(createCustomerDto);
   }
 
+  // Edit a customer by ID ++++++++++++++++++++++++++++++++
   @Patch('/edit/:customerId')
+  @ApiOperation({ summary: 'Update customer by ID' })
+  @ApiParam({ name: 'customerId', type: Number })
+  @ApiBody({ type: CreateCustomerDto })
+  @ApiResponse({ status: 200, type: Customer })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   public async editCustomer(
     @Body() createCustomerDto: CreateCustomerDto,
     @Param('customerId') customerId: number,
@@ -59,7 +180,12 @@ export class CustomerController {
     );
   }
 
+  // Delete a customer by ID +++++++++++++++++++++++++++++
   @Delete('/delete/:customerId')
+  @ApiOperation({ summary: 'Delete customer by ID' })
+  @ApiParam({ name: 'customerId', type: Number })
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   public async deleteCustomer(@Param('customerId') customerId: number) {
     return await this.customerService.deleteCustomer(customerId);
   }
--- src/customer/dtos/create-customer.dto.ts
+++ src/customer/dtos/create-customer.dto.ts
@@ -1,46 +1,61 @@
 import { IsNotEmpty, IsString, IsNumber, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateCustomerDto {
   @IsString()
+  @ApiProperty()
   sosVoterId: string;
 
   @IsNumber()
   @IsNotEmpty()
   @MaxLength(25)
+  @ApiProperty()
   idNumber: number;
 
   @IsString()
+  @ApiProperty()
   voterStatus: string;
 
   @IsString()
+  @ApiProperty()
   partyCode: string;
 
   @IsString()
+  @ApiProperty()
   firstName: string;
 
   @IsString()
+  @ApiProperty()
   middleName: string;
 
   @IsString()
+  @ApiProperty()
   lastName: string;
 
   @IsString()
+  @ApiProperty()
   sex: string;
 
   @IsString()
+  @ApiProperty()
   birthDate: string;
 
   @IsString()
+  @ApiProperty()
   streetName: string;
 
   @IsString()
+  @ApiProperty()
   streetNumber: string;
 
   @IsString()
+  @ApiProperty()
   streetType: string;
 
   @IsString()
+  @ApiProperty()
   city: string;
 
   @IsString()
+  @ApiProperty()
   zipCode: string;
 }
