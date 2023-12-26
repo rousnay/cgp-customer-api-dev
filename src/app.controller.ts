2bc775466086579ae28e1da679b4b5c6073ac0c4
Mozahidur Rahman <mr.rousnay@gmail.com>
2023-12-26 13:42:06 +0600

    User login added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,65 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiBody, ApiConsumes, ApiTags, ApiQuery } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiConsumes,
+  ApiTags,
+  ApiQuery,
+} from '@nestjs/swagger';
 
 @Controller()
-@ApiTags('App')
+@ApiTags('Login')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  @ApiOperation({ summary: 'User Login' })
+  @ApiBody({
+    description: 'User login credentials',
+    examples: {
+      credentials: {
+        summary: 'Example of valid login credentials',
+        value: {
+          username: 'john_doe',
+          password: 'my_password',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    // type: Customer,
+    description: 'Logged in successfully',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            message: { type: 'string', example: 'This is an example message' },
+            data: {
+              type: 'object',
+              properties: {
+                userId: { type: 'number', example: 1 },
+                username: { type: 'string', example: 'john_doe' },
+              },
+            },
+          },
+        },
+        example: { userId: 1, username: 'john' },
+      },
+    },
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async login(@Request() req) {
+    return req.user;
   }
 }
