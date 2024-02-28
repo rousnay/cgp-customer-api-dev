cd4097f21dc4cdf3b724edef95353d872041ae87
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-02-28 19:37:35 +0600

    Working for CORS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
-// import { CorsOptions } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.enableCors();
-
-  // OR for more advanced options
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -17,14 +14,6 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     credentials: true,
   });
-
-  //   const corsOptions: CorsOptions = {
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type,Authorization',
-  //   credentials: true,
-  // };
-  // app.enableCors(corsOptions);
 
   setupSwagger(app);
   await app.listen(3000);
