5c1dd5b0675874f71a09e91ec44eaccfa428a24a
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-04-08 10:12:46 +0600

    Customer API completed 1

---
+++ src/users/users.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+// import { UsersService } from './users.service';
+// import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+// import { PasswordService } from '../auth/password.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Users])],
+  //   exports: [UsersService],
+  //   providers: [UsersService, PasswordService],
+  //   controllers: [UsersController],
+})
+export class UsersModule {}
