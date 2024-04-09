cab9a282624d99914ce5a4258a182f3f63a3522a
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-04-09 13:17:13 +0600

    Working with customer API 3

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customers } from './entities/customers.entity'; // Import your CustomerEntity
 import { CustomersService } from './services/customers.service';
+import { CustomerAddressBook } from './entities/customer_address_book.entity';
 import { CustomerController } from './controllers/customers.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customers])], // Import the entities
+  imports: [TypeOrmModule.forFeature([Customers, CustomerAddressBook])], // Import the entities
   exports: [CustomersService],
   providers: [CustomersService], // Import your service
   controllers: [CustomerController], // Optionally export service if needed in other modules
