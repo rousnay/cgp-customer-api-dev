3fb124f7d2360ddabfc8fc5ef5ec9418f037b5ab
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-05-07 12:29:37 +0600

    Vehicle image url added

--- src/transportations/dtos/transportation-vehicle.dto.ts
+++ src/transportations/dtos/transportation-vehicle.dto.ts
@@ -1,12 +1,14 @@
 export class TransportationVehicleDto {
   id: number;
   type_id: number;
+  name: string;
+  code: string;
+  media_url: string;
   vehicle_capacity: number;
   min_km: number;
   min_fare: number;
   per_km_fare: number;
   max_time_in_minutes: number;
   per_minutes_fare: number;
-  cf_media_id: number;
   active: boolean;
 }
--- src/transportations/services/transportation-vehicles.service.ts
+++ src/transportations/services/transportation-vehicles.service.ts
@@ -6,6 +6,9 @@ import { TransportationVehicleDto } from '../dtos/transportation-vehicle.dto';
 // Define a class representing your transportation vehicles service
 @Injectable()
 export class TransportationVehiclesService {
+  private readonly base_url = 'https://imagedelivery.net';
+  private readonly account_hash = 'GoCn5FC83XAthwamRI9Rdg';
+  private readonly variant = 'public';
   constructor(
     @InjectEntityManager() private readonly entityManager: EntityManager,
   ) {}
@@ -14,19 +17,10 @@ export class TransportationVehiclesService {
     try {
       const query = await this.entityManager
         .createQueryBuilder()
-        .select([
-          'id',
-          'type_id',
-          'vehicle_capacity',
-          'min_km',
-          'min_fare',
-          'per_km_fare',
-          'max_time_in_minutes',
-          'per_minutes_fare',
-          'cf_media_id',
-          'active',
-        ])
+        .select(['vt.*', 's.name', 's.code', 'm.cloudflare_id', 'm.file'])
         .from('vehicle_types', 'vt')
+        .leftJoin('statuses', 's', 'vt.type_id = s.id')
+        .leftJoin('cf_media', 'm', 'vt.cf_media_id = m.id')
         .getRawMany();
 
       // Check if no data found
@@ -34,25 +28,32 @@ export class TransportationVehiclesService {
         throw new NotFoundException('No transportation vehicles found.');
       }
 
-      return query; //to response with full results
-
       // Map raw results to DTO to return specific fields
-      // const transportationVehicles: TransportationVehicleDto[] = query.map(
-      //   (row) => ({
-      //     id: row.id,
-      //     type_id: row.type_id,
-      //     vehicle_capacity: row.vehicle_capacity,
-      //     min_km: row.min_km,
-      //     min_fare: row.min_fare,
-      //     per_km_fare: row.per_km_fare,
-      //     max_time_in_minutes: row._max_time_in_minutes,
-      //     per_minutes_fare: row.per_minutes_fare,
-      //     cf_media_id: row.cf_media_id,
-      //     active: row.active,
-      //   }),
-      // );
+      const transportationVehicles: TransportationVehicleDto[] = query.map(
+        (row) => ({
+          id: row.id,
+          type_id: row.type_id,
+          name: row.name,
+          code: row.code,
+          media_url:
+            this.base_url +
+            '/' +
+            this.account_hash +
+            '/' +
+            row.cloudflare_id +
+            '/' +
+            this.variant,
+          vehicle_capacity: row.vehicle_capacity,
+          min_km: row.min_km,
+          min_fare: row.min_fare,
+          per_km_fare: row.per_km_fare,
+          max_time_in_minutes: row._max_time_in_minutes,
+          per_minutes_fare: row.per_minutes_fare,
+          active: row.active,
+        }),
+      );
 
-      // return transportationVehicles;
+      return transportationVehicles;
     } catch (error) {
       // Log the error and rethrow it for the calling code to handle
       console.error('Error fetching transportation vehicles:', error);
@@ -64,19 +65,10 @@ export class TransportationVehiclesService {
     try {
       const query = await this.entityManager
         .createQueryBuilder()
-        .select([
-          'id',
-          'type_id',
-          'vehicle_capacity',
-          'min_km',
-          'min_fare',
-          'per_km_fare',
-          'max_time_in_minutes',
-          'per_minutes_fare',
-          'cf_media_id',
-          'active',
-        ])
+        .select(['vt.*', 's.name', 's.code', 'm.cloudflare_id', 'm.file'])
         .from('vehicle_types', 'vt')
+        .leftJoin('statuses', 's', 'vt.type_id = s.id')
+        .leftJoin('cf_media', 'm', 'vt.cf_media_id = m.id')
         .where('vt.type_id = :id', { id })
         .getRawOne();
 
@@ -84,7 +76,33 @@ export class TransportationVehiclesService {
         throw new NotFoundException('Vehicle not found');
       }
 
-      return query;
+      const media_url =
+        this.base_url +
+        '/' +
+        this.account_hash +
+        '/' +
+        query.cloudflare_id +
+        '/' +
+        this.variant;
+
+      const transportationVehicle: TransportationVehicleDto = {
+        id: query.id,
+        type_id: query.type_id,
+        name: query.name,
+        code: query.code,
+        media_url: media_url,
+        vehicle_capacity: query.vehicle_capacity,
+        min_km: query.min_km,
+        min_fare: query.min_fare,
+        per_km_fare: query.per_km_fare,
+        max_time_in_minutes: query._max_time_in_minutes,
+        per_minutes_fare: query.per_minutes_fare,
+        active: query.active,
+      };
+
+      return transportationVehicle;
+
+      // return query;
     } catch (error) {
       // Log the error and rethrow it for the calling code to handle
       console.error(
