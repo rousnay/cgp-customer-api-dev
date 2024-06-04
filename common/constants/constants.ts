bc20c29e279dec6da50a7aeb47a16b02ffbfcdcd
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-06-04 12:12:18 +0600

    Retry 4

--- common/constants/constants.ts
+++
@@ -1,7 +0,0 @@
-export const AppConstants = {
-  cloudflare: {
-    mediaVariant: 'public',
-    mediaBaseUrl: 'https://imagedelivery.net',
-    apiBaseUrl: 'https://api.cloudflare.com/client/v4/accounts',
-  },
-};
