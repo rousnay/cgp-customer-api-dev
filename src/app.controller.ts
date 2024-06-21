75189cd435e33171bb89f780695d7e8456d2e27b
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-06-21 13:29:17 +0600

    Ready for render 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+  async totalRiders(): Promise<any> {
+    return this.appService.getHelloAsync();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectEntityManager() private readonly entityManager: EntityManager,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getHelloAsync(): Promise<string> {
+    const rawQuery = `
+        SELECT COUNT(*) AS total_customers
+        FROM customers;
+        `;
+
+    const result = await this.entityManager.query(rawQuery);
+
+    if (result.length === 0) {
+      return null;
+    }
+    return 'Total customers: ' + result[0].total_customers;
   }
 }
