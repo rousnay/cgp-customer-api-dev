19f5f7c01feafc97ebae0b8b205fbbd19514241e
Mozahidur Rahman <mr.rousnay@gmail.com>
2024-07-15 19:27:40 +0600

    working with post payment api

--- src/modules/orders/services/transportation-orders.service.ts
+++ src/modules/orders/services/transportation-orders.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
@@ -28,9 +28,6 @@ export class TransportationOrdersService {
     const customer = this.request['user'];
     let pickup_address_id: number;
     let shipping_address_id: number;
-    let stripeSession: any;
-    let stripePaymentIntent: any;
-    let stripe_id: any;
 
     if (createTransportationOrderDto.pickup_address_id) {
       pickup_address_id = createTransportationOrderDto.pickup_address_id;
@@ -61,50 +58,17 @@ export class TransportationOrdersService {
       throw new Error('Transportation order not created');
     }
 
-    const processPayment = {
-      payable_amount: order.payable_amount,
-      // shipping_address: createTransportationOrderDto.shipping_address,
-      // pickup_address_coordinates:
-      //   createTransportationOrderDto.pickup_address.latitude.toString() +
-      //   ',' +
-      //   createTransportationOrderDto.pickup_address.longitude.toString(),
-      // shipping_address_coordinates:
-      //   createTransportationOrderDto.shipping_address.latitude.toString() +
-      //   ',' +
-      //   createTransportationOrderDto.shipping_address.longitude.toString(),
-      // vehicle_type_id: createTransportationOrderDto.vehicle_type_id,
-      total_cost: createTransportationOrderDto.total_cost,
-      gst: createTransportationOrderDto.gst,
-    };
-
-    if (payment_client === 'web') {
-      stripeSession = await this.paymentService.createCheckoutSession(
-        processPayment,
-      );
-      stripe_id = stripeSession?.id;
-    } else if (payment_client === 'app') {
-      stripePaymentIntent = await this.paymentService.createPaymentIntent(
-        processPayment,
-      );
-
-      // Remove secret and get only payment Intent
-      const regex = /^(.*?)_secret/;
-      const match = stripePaymentIntent?.client_secret.match(regex);
-      stripe_id = match[1];
-    }
-
     const savedOrder = await this.transportationOrdersRepository.save(order);
 
     const savedPayment = await this.paymentService.storePaymentStatus(
       savedOrder?.id,
-      stripe_id,
+      null,
       'Pending',
     );
 
     const savedDelivery = await this.deliveriesRepository.save({
       customer_id: customer?.id,
       order_id: savedOrder?.id,
-      // vehicle_type_id: createTransportationOrderDto?.vehicle_type_id,
       init_distance: createTransportationOrderDto?.distance,
       init_duration: createTransportationOrderDto?.duration,
       delivery_charge: createTransportationOrderDto?.payable_amount,
@@ -128,8 +92,6 @@ export class TransportationOrdersService {
       order: orderInfo,
       delivery: savedDelivery,
       payment: savedPayment,
-      session: stripeSession,
-      PaymentIntent: stripePaymentIntent,
     };
   }
 }
